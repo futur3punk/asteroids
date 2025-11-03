7320f822527406ecb36d8ec6c23f2fb63d007fc1
pupi <futurepunk0828@gmail.com>
2025-11-03 19:44:31 +0100

    player added to playing field

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from constants import *
 import pygame
+from player import *
+from circleshape import *
 
 def main():
     
@@ -7,15 +9,27 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock=pygame.time.Clock()
+    dt = 0
+    
     while True: 
         for event in pygame.event.get():
               if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        player.draw(screen)
+        
         pygame.display.flip()
+        
+        #limit the frarate to 60 fps
+        #clock.tick(60)
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
